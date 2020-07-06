97e6095be8def2e3174944e787cb2aa2972eb252
fernando.silva <fernandosilvaudi@gmail.com>
2020-07-06 11:55:04 -0300

    Encapsulando variaveis ambiente com o dotenv

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import accountsRouter from './routes/accountsRouter.js';
+import 'dotenv/config.js'; // encontrei no github
+
+// import dotenv from 'dotenv'; //encontrei no npm
+// dotenv.config();
 
 // Conexão com o banco de dados mongoose
 (async () => {
   try {
     console.log('conectando');
+    console.log(process.env.USERDB);
+    console.log(process.env.PWDDB);
+    console.log(process.env.PORT);
+
     await mongoose.connect(
-      'mongodb+srv://fernandosilvaudi:V2krLbOl8H71STdt@bootcamp.zqebg.gcp.mongodb.net/TrabalhoPratico?retryWrites=true&w=majority',
+      // 'mongodb+srv://fernandosilvaudi:V2krLbOl8H71STdt@bootcamp.zqebg.gcp.mongodb.net/TrabalhoPratico?retryWrites=true&w=majority',
+      `mongodb+srv://${process.env.USERDB}:${process.env.PWDDB}@bootcamp.zqebg.gcp.mongodb.net/TrabalhoPratico?retryWrites=true&w=majority`,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -25,6 +34,6 @@ const app = express();
 app.use(express.json());
 app.use('/account', accountsRouter);
 
-app.listen(3000, async () => {
+app.listen(process.env.PORT, async () => {
   console.log('Iniciando');
 });
